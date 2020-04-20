b2163812c61d46b900cc8e9d714bb823ba515567
Emily Hudson <emily33901@gmail.com>
2020-04-20 18:36:14 +0100

    Add some test scene code and begin work on test wireframe rendering

--- core/scenes/go.mod
+++ core/scenes/go.mod
@@ -1,3 +1,5 @@
 module github.com/emily33901/forgery/maps
 
 go 1.14
+
+require github.com/g3n/engine v0.1.0
--- core/scenes/scene.go
+++ core/scenes/scene.go
@@ -10,7 +10,7 @@ import (
 
 // Scene maps a map file (vmf) and a g3n scene together
 // (it technically doesnt do this but it keeps track of
-//  geomentry and similar and allows for exporting back
+//  geometry and similar and allows for exporting back
 //  to a vmf)
 
 type Scene struct {
@@ -27,13 +27,99 @@ func New() *Scene {
 	s.Geom = s.Root.Add(core.NewNode())
 	s.Debug = s.Root.Add(core.NewNode())
 
-	geom := geometry.NewPlane(10, 10)
+	// geom := geometry.NewBox(10, 10, 10)
 
-	mesh := graphic.NewMesh(geom, material.NewStandard(math32.NewColorHex(0x55331122)))
+	// box := geom.BoundingBox()
+
+	// var boxSize math32.Vector3
+	// box.Size(&boxSize)
+
+	// lineMaterial := material.NewStandard(math32.NewColorHex(0xAABBCC))
+
+	// lineMaterial.SetSide(material.SideDouble)
+	// // lineMaterial.SetUseLights(material.)
+	// // lineMaterial.SetWireframe(true)
+
+	// // Get all the edges of the box
+	// pointList := []*math32.Vector3{
+	// 	// 0
+	// 	&box.Min,
+
+	// 	// 1
+	// 	box.Min.Clone().Add(&math32.Vector3{boxSize.X, 0, 0}),
+	// 	box.Min.Clone().Add(&math32.Vector3{0, boxSize.Y, 0}),
+	// 	box.Min.Clone().Add(&math32.Vector3{0, 0, boxSize.Z}),
+
+	// 	//4
+	// 	box.Min.Clone().Add(&math32.Vector3{boxSize.X, boxSize.Y, 0}),
+	// 	box.Min.Clone().Add(&math32.Vector3{0, boxSize.Y, boxSize.Z}),
+	// 	box.Min.Clone().Add(&math32.Vector3{boxSize.X, 0, boxSize.Z}),
+
+	// 	// 7
+	// 	&box.Max,
+	// }
+
+	// edgeList := [][]int{
+	// 	{0, 1},
+	// 	{0, 2},
+	// 	{0, 3},
+
+	// 	{1, 4},
+	// 	{1, 6},
+
+	// 	{2, 4},
+	// 	{2, 5},
+
+	// 	{3, 5},
+	// 	{3, 6},
+
+	// 	{4, 7},
+	// 	{5, 7},
+	// 	{6, 7},
+	// }
+
+	// // Now create the lines
+	// lines := []*graphic.Lines{}
+	// for _, e := range edgeList {
+	// 	p1 := pointList[e[0]]
+	// 	p2 := pointList[e[1]]
+
+	// 	lineGeom := NewLine(p1.DistanceTo(p2))
+
+	// 	l := graphic.NewLines(lineGeom, material.NewBasic())
+
+	// 	l.SetPositionVec(p1.Clone().Add(&math32.Vector3{-boxSize.X, -boxSize.Y, -boxSize.Z}))
+	// 	l.SetRotationVec(p1.Clone().Sub(p2).Normalize())
+
+	// 	lines = append(lines, l)
+	// }
+
+	// for _, l := range lines {
+	// 	s.Geom.Add(l)
+	// }
+
+	p := geometry.NewPlane(0, 10)
+
+	defaultMat := material.NewStandard(math32.NewColorHex(0xAABBCC))
+	defaultMat.SetWireframe(true)
+	defaultMat.SetSide(material.SideDouble)
+
+	graphic.NewLines(p, material.NewStandard(math32.NewColorHex(0xAABBCC)))
+
+	mesh := graphic.NewMesh(p, defaultMat)
 	mesh.SetPosition(0, 0, 0)
 	mesh.SetRotation(10, 10, 10)
 
 	s.Geom.Add(mesh)
 
 	return s
+}
+
+func NewLine(length float32) (line *geometry.Geometry) {
+	if length <= 0 {
+		panic("Length <= 0")
+	}
+
+	line = geometry.NewPlane(0, length)
+	return
 }
